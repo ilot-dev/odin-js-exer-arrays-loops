be39ad6ed5f28d265ee2a1f97b39efab857a59c6
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-03-27 19:42:09 +0800

    Edit files to pass tests 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
     let repeatedText = '';
-    for (let i = 0; i < 3; i++) repeatedText += 'hey';
+    for (let i = 0; i < num; i++) repeatedText += text;
     return repeatedText;
 };
 
+repeatString('hey', 3);
+repeatString('hello', 10);
 // Do not edit below this line
 module.exports = repeatString;
