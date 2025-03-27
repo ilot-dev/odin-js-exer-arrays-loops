d375ba1daaa800ac96c8fec1394466e3b944b649
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-03-27 19:37:38 +0800

    Edit repeatString.js to pass test #1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
 const repeatString = function() {
-
+    let repeatedText = '';
+    for (let i = 0; i < 3; i++) repeatedText += 'hey';
+    return repeatedText;
 };
 
 // Do not edit below this line
