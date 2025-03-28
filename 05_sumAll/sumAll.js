6604d8a4da060a792baff8e40880bfd17ba29962
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-03-28 17:17:20 +0800

    Edit files to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (int1 >= 0 && int2 >= 0 && 
+        Number.isInteger(int1) && 
+        Number.isInteger(int2) &&
+        !isNaN(int1) &&
+        !isNaN(int2)) {
+            let numberOfTerms = Math.abs(int2 - int1) + 1;
 
+             // Formula for sum of consecutive numbers
+            let sum = (numberOfTerms / 2) * (int1 + int2);
+            return sum;
+        } 
+        else return 'ERROR';
+    
 };
 
 // Do not edit below this line
