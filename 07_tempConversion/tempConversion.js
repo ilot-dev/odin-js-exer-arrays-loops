b036911961bb0b6065c75a943b96bc44ae54ce8d
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-03-28 18:15:05 +0800

    Edit files to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsiusTemp = (temperature - 32) / 1.8;
+  let roundedCelsiusTemp = parseFloat(celsiusTemp.toFixed(1));
+  return roundedCelsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheitTemp = temperature * 1.8 + 32;
+  let roundedFahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1));
+  return roundedFahrenheitTemp;
 };
 
 // Do not edit below this line
