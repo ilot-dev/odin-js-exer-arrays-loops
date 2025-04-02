9d5f0aa0ccb9c3f0ed92e853d28d09d903ea40f2
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-04-02 17:58:41 +0800

    Edit all files to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.map((person) => {
+        if ('yearOfDeath' in person) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            let currentYear = new Date().getFullYear();
+            person['age'] = currentYear - person.yearOfBirth;
+        }
+    });
 
+    let currentOldestAge = 0;
+    let oldestPerson;
+    for (person of people){
+        if (person.age > currentOldestAge) {
+            currentOldestAge = person.age
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
+    
 };
 
 // Do not edit below this line
