9132ea67db318203a9097c94e6309af26db0490f
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-04-02 17:10:32 +0800

    Edit files to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = parseInt(input);
+    if (num < 0) return 'OOPS';
+    else if (num === 0 || num === 1) return num;
 
+    const fibonacciArray = [0, 1];
+    let fibonacciSum;
+    for (let i = 1; i < num; ++i){
+        fibonacciSum = fibonacciArray[i - 1] + fibonacciArray[i];
+        fibonacciArray.push(fibonacciSum);
+    }
+    return fibonacciSum;
 };
 
 // Do not edit below this line
