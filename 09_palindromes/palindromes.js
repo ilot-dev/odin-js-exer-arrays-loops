df3c154818b2768827f89b692258a81f45fdc48d
Ilot-Dev <168155871+ilot-dev@users.noreply.github.com>
2025-04-02 16:28:20 +0800

    Edit all files to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const textWithoutPunctuation = [...text].filter(char => /[^\W_]/.test(char)).join('');
+    const textLowerCase = textWithoutPunctuation.toLowerCase();
+    const reversedTextLowerCase = textLowerCase.split('').reverse().join('');
 
+    return textLowerCase === reversedTextLowerCase;
 };
 
 // Do not edit below this line
